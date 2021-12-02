6e51a21cfe8ca18940bf869deddbe9ed53f20dab
RoseMarieW <Rose.Wilson@tcgplayer.com>
2021-12-02 07:40:02 -0500

    int array instead of string array

--- day01/main.go
+++ day01/main.go
@@ -2,17 +2,19 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"log"
 	"os"
+	"strconv"
 )
 
 func main() {
 	text := readFile()
 	part1 := part1(text)
-	println(part1)
+	fmt.Println(part1)
 }
 
-func part1(text []string) int {
+func part1(text []int) int {
 	numIncreases := 0
 	for i := 1; i < len(text); i++ {
 		if text[i] > text[i-1] {
@@ -22,7 +24,7 @@ func part1(text []string) int {
 	return numIncreases
 }
 
-func readFile() []string {
+func readFile() []int {
 	file, err := os.Open("input.txt")
 
 	if err != nil {
@@ -32,10 +34,20 @@ func readFile() []string {
 	scanner := bufio.NewScanner(file)
 
 	scanner.Split(bufio.ScanLines)
-	var text []string
+	var text []int
 
 	for scanner.Scan() {
-		text = append(text, scanner.Text())
+		text = append(text, convert(scanner.Text()))
 	}
 	return text
+}
+
+func convert(str string) int {
+	i, err := strconv.Atoi(str)
+	if err != nil {
+		// handle error
+		fmt.Println(err)
+		os.Exit(2)
+	}
+	return i
 }
