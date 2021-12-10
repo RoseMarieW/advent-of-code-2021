20cfa7ded8673c80c4337f6307101dd57918ae7e
RoseMarieW <Rose.Wilson@tcgplayer.com>
2021-12-09 21:47:52 -0500

    and this is where i realize my grave mistake
    putting this here for education.
    see, sometimes it ends before it reaches the end

--- day03/main.go
+++ day03/main.go
@@ -11,10 +11,49 @@ import (
 
 func main() {
 	text := readFile()
-	part1 := part1(text)
-	fmt.Println(part1)
-	//part2 := part2(text)
-	//fmt.Println(part2)
+	//part1 := part1(text)
+	//fmt.Println(part1)
+	part2 := part2(text)
+	fmt.Println(part2)
+}
+
+func part2(text []string) string {
+	//for those reading I did part1 a week ago, got sick, and now I hate how I did part1 sooo lets retry this
+	
+	//dont want to mess with original input
+	oxygen := text
+	oxygenRating := ""
+	position :=0
+	//Oxygen Generator Rating
+	for len(oxygen) >1{
+		//find maximum
+		max := mostCommonValue(oxygen, position)
+		//discard minimum
+		oxygen = discard(oxygen, position, max)
+		//add value to rating
+		oxygenRating += max
+		position++
+	}
+
+	//CO2 Scrubber Rating
+	//dont want to mess with original input
+	co := text
+	coRating := ""
+	position = 0
+	//Oxygen Generator Rating
+	for len(co) >1{
+		//find min
+		min := leastCommonValue(co, position)
+		println(min)
+		//discard min
+		co = discard(co, position, min)
+		//add value to rating
+		coRating += min
+		position++
+	}
+
+	println(coRating)
+	return oxygenRating
 }
 
 func part1(text []string) int64 {
@@ -60,9 +99,6 @@ func part1(text []string) int64 {
 	return gammarate*epsilonrate
 }
 
-//func part2(text []string) int {
-//}
-
 func readFile() []string {
 	file, err := os.Open("testinput.txt")
 
@@ -89,4 +125,54 @@ func convert(str string) int {
 		os.Exit(2)
 	}
 	return i
+}
+
+func mostCommonValue(report []string, position int) string{
+	one := 0
+	zero := 0
+	for _, diagnostic := range report {
+		if diagnostic[position] == '1' {
+			one++
+		} else {
+			zero++
+		}
+	}
+
+	if one>=zero{
+		return "1"
+	} else {
+		return "0"
+	}
+
+}
+
+//this is redundant
+func leastCommonValue(report []string, position int) string{
+	one := 0
+	zero := 0
+	for _, diagnostic := range report {
+		if diagnostic[position] == '1' {
+			one++
+		} else {
+			zero++
+		}
+	}
+
+	if one<zero{
+		return "1"
+	} else {
+		return "0"
+	}
+
+}
+
+func discard(report []string, position int, keep string) []string{
+	var kept []string
+	for index, diagnostic := range report {
+		//this seems...
+		if diagnostic[position] == []byte(keep)[0] {
+			kept = append(kept,report[index])
+		}
+	}
+	return kept
 }
