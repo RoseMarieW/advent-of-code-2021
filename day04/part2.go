51683b103f6c88d7995d801fc52780d9f8c28640
RoseMarieW <Rose.Wilson@tcgplayer.com>
2021-12-10 20:13:19 -0500

    day 4

--- day04/part2.go
+++ day04/part2.go
@@ -12,39 +12,30 @@ import (
 func main() {
 	board := readBoard()
 	bingoCalls := readBingoNums()
-	part1 := part1(board, bingoCalls)
-	fmt.Println(part1)
-	//part2 := part2(text)
-	//fmt.Println(part2)
+	part2 := part2(board, bingoCalls)
+	fmt.Println(part2)
 }
 
-//func part2(text []string) int64 {
-//}
-
-func part1(board []string, bingoCalls []string) int {
+func part2(board []string, bingoCalls []string) int {
 	//assume that it might take all bingoCalls
-	minCalls := len(bingoCalls)
+	maxCalls := 0
 	winScore := 0
 	//a board is 25
 	for i:=0; i<len(board); i += 25{
-		calls,score := contains(board[i:i+25],bingoCalls,minCalls)
-		//I dont think I need to check for min again because I check in contains (thats a bad name)
+		calls,score := contains(board[i:i+25],bingoCalls,maxCalls)
+		//I dont think I need to check for max again because I check in contains (thats a bad name)
 		if(calls != -1){
-			minCalls = calls
+			maxCalls = calls
 			winScore = score
 		}
 	}
 	return winScore
 }
 
-func contains(board []string, bingoCalls []string, minCalls int) (int,int){
+func contains(board []string, bingoCalls []string, maxCalls int) (int,int){
 	//for each call check if it is on board
 	var tempBoard [25]int
 	for i:=0;i<len(bingoCalls);i++{
-		//throw away board if loser
-		if i>minCalls{
-			return -1,-1
-		}
 		for j:=0;j<len(board);j++{
 			//if contains
 			if board[j] == bingoCalls[i]{
@@ -54,12 +45,12 @@ func contains(board []string, bingoCalls []string, minCalls int) (int,int){
 				if i>=5{
 					//if there is a winner
 					if isWinner(tempBoard){
-						//check if this winner is less than any other minimum calls
-						if i<minCalls{
-							minCalls = i
+						//check if this winner is greater than any other maximum calls
+						if i>maxCalls{
+							maxCalls = i
 							score := getScore(board,tempBoard,convert(bingoCalls[i]))
-							return minCalls, score
-						}
+							return maxCalls, score
+						}else{return -1,-1} 
 					}
 				}
 			}
@@ -71,14 +62,14 @@ func contains(board []string, bingoCalls []string, minCalls int) (int,int){
 
 func isWinner(tempBoard [25]int) bool{
 	//horizontals
-	for i:=0;i<=20;i+=5{
+	for i:=0;i<25;i+=5{
 		if (tempBoard[i] + tempBoard[i+1] + tempBoard[i+2] + tempBoard[i+3] + tempBoard[i+4])==5{
 			return true
 		}
 	}
 
 	//verticals
-	for i:=0;i<=4;i++{
+	for i:=0;i<5;i++{
 		if (tempBoard[i] + tempBoard[i+5] + tempBoard[i+10] + tempBoard[i+15] + tempBoard[i+20])==5{
 			return true
 		} 
@@ -111,7 +102,6 @@ func readBoard() []string {
 	for scanner.Scan() {
 		text = append(text, scanner.Text())
 	}
-	fmt.Println(text)
 	return text
 }
 
@@ -130,7 +120,6 @@ func readBingoNums() []string {
 	for scanner.Scan() {
 		text = strings.Split(scanner.Text(),",")
 	}
-	fmt.Println(text)
 	return text
 }
 
